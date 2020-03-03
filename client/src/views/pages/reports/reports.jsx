623c58aefff55c8ec1a5858edb893e3775f7e57f
StructByLightning <mossygreenrock@gmail.com>
2020-03-03 11:45:02 -0800

    an insignificant chance

--- client/src/views/pages/reports/reports.jsx
+++ client/src/views/pages/reports/reports.jsx
@@ -9,8 +9,8 @@ export default class Reports extends React.Component {
   render() {
     return (
       <div className="reports">
-        <div>You are on the: reports page.</div>
-        <div>Select a report:</div>
+        <div>You are on the: reports page. </div>
+        <div>Select a Report:</div>
         <nav>
           <Link className="button" to="/reports/attendance">Attendance Report</Link>
           <Link className="button" to="/reports/sales">Sales Report</Link>
