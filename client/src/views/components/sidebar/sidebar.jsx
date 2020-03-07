95bd0b1f8083cff2bf5a1642d8d9541e8a590f48
Raven <mossygreenrock@gmail.com>
2020-03-07 09:56:49 -0800

    add sidebar

--- client/src/views/components/sidebar/sidebar.jsx
+++ client/src/views/components/sidebar/sidebar.jsx
@@ -1,14 +1,34 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./sidebar.scss";
+import {ReactComponent as HomeIcon} from "svg/icons/home.svg";
+import {ReactComponent as ReportsIcon} from "svg/icons/reports.svg";
+import {ReactComponent as DevIcon} from "svg/icons/dev.svg";
+import {ReactComponent as OptionsIcon} from "svg/icons/options.svg";
 
 export default class Sidebar extends React.Component {
   render = () => {
     return (
-      <div className="sidebar">
-        <Link className="button" to="/home">Home</Link>
-        <Link className="button" to="/reports">Reports</Link>
-      </div>
+      <aside className="sidebar">
+        <div className="prominent-items">
+          <NavLink to="/home" className="nav-item" activeClassName="active">
+            <HomeIcon className="icon"/>
+            <span className="label">Home</span>
+          </NavLink>
+          <NavLink to="/reports" className="nav-item" activeClassName="active">
+            <ReportsIcon className="icon"/>
+            <span className="label">Reports</span>
+          </NavLink>
+          <NavLink to="/dev" className="nav-item" activeClassName="active">
+            <DevIcon className="icon"/>
+            <span className="label">Dev</span>
+          </NavLink>
+        </div>
+        <NavLink to="/options" className="nav-item" activeClassName="active">
+          <OptionsIcon className="icon"/>
+          <span className="label">Options</span>
+        </NavLink>
+      </aside>
     );
   }
 }
