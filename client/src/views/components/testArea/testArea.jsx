7113802d3f80a3dee3d8b439560da814bd37e569
Raven <mossygreenrock@gmail.com>
2020-03-21 11:01:36 -0700

    Add checkboxes to testing area

--- client/src/views/components/testArea/testArea.jsx
+++ client/src/views/components/testArea/testArea.jsx
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom";
 import "./testArea.scss";
 import PropTypes from "prop-types";
 import Button from "views/components/button/button.jsx";
+import Checkbox from "views/components/checkbox/checkbox.jsx";
 
 export default class JsonViewer extends React.Component {
   render = () => {
@@ -12,6 +13,10 @@ export default class JsonViewer extends React.Component {
         <Button destructive label="Delete" onClick={() => {}}/>
         <Button destructive label="Delete" onClick={() => {}}/>
         <Button destructive label="Delete" onClick={() => {}}/>
+        <Checkbox label="tick me" onChange={(val) => {
+          console.log("Checkbox changed", val);
+        }}/>
+        <Checkbox label="untouchable" disabled />
       </div>
     );
   }
