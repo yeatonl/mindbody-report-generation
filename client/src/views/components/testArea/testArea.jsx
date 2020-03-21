2a83dbdb1aa592e829fc0213b75242ec1de647c6
Raven <mossygreenrock@gmail.com>
2020-03-21 10:10:57 -0700

    Add destructive buttons to the test area

--- client/src/views/components/testArea/testArea.jsx
+++ client/src/views/components/testArea/testArea.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "./testArea.scss";
 import PropTypes from "prop-types";
+import Button from "views/components/button/button.jsx";
 
 export default class JsonViewer extends React.Component {
   render = () => {
     return (
-      <div className="json-viewer">
-        JSON viewer
+      <div className="test-area">
+        <Button destructive label="Delete" onClick={() => {}}/>
+        <Button destructive label="Delete" onClick={() => {}}/>
+        <Button destructive label="Delete" onClick={() => {}}/>
+        <Button destructive label="Delete" onClick={() => {}}/>
       </div>
     );
   }
