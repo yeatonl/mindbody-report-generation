7b1ad02c7ed3243f5cb2b2dcb9cd3005314b2616
Raven <mossygreenrock@gmail.com>
2020-04-03 08:21:35 -0700

    remove unnecessary code

--- client/src/views/pages/home/home.jsx
+++ client/src/views/pages/home/home.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { connect } from "react-redux";
-import store from "store/store.js";
 
 import * as Actions from "store/actions/index.js";
 import * as themes from "constants/themes.js";
@@ -11,16 +10,13 @@ import "./home.scss";
 export default connect((state) => {
   return {
   };
-}, () => {
-  return {};
-})(class Home extends React.Component {
+}, null)(class Home extends React.Component {
   render = () => {
     return (
       <div className="home">
         <div>You are on the: homepage</div>
         <button className="button" onClick={() => {
           Actions.setTheme(themes.LIGHT);
-          console.log(store.getState());
         }}>Change redux data</button>
       </div>
     );
