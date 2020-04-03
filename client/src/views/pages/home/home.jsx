389722e99e3648f8e65f1ad4722f120cba90faa6
Raven <mossygreenrock@gmail.com>
2020-04-03 08:19:54 -0700

    add settings reducer

--- client/src/views/pages/home/home.jsx
+++ client/src/views/pages/home/home.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { connect } from "react-redux";
-
 import store from "store/store.js";
+
 import * as Actions from "store/actions/index.js";
+import * as themes from "constants/themes.js";
 
 import "./home.scss";
 
 
 export default connect((state) => {
   return {
-    example: state.example,
   };
 }, () => {
   return {};
@@ -18,9 +18,9 @@ export default connect((state) => {
     return (
       <div className="home">
         <div>You are on the: homepage</div>
-        <div>Redux data: {String(this.props.example.exampleField)}</div>
         <button className="button" onClick={() => {
-          store.dispatch(Actions.EXAMPLE_ACTION());
+          Actions.setTheme(themes.LIGHT);
+          console.log(store.getState());
         }}>Change redux data</button>
       </div>
     );
