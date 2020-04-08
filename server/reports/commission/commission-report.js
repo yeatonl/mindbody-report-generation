2c0fa826ed630f8fcdecbdae30bedb8a55470423
Daniel Brewer <44036901+DanielBrewerPsu@users.noreply.github.com>
2020-04-07 17:23:28 -0700

    Rename varibles: req to request and res to response

--- server/reports/commission/commission-report.js
+++ server/reports/commission/commission-report.js
@@ -5,32 +5,32 @@ const { Parser } = require("json2csv");
 /* A handy request URLS for testing:
 http://localhost:8080/reports/commission?format=csv&startdate=111&enddate=2222
 */
-function handleExpressRequest(req, res) {
+function handleExpressRequest(request, response) {
 
-  console.log("handleExpressRequest:");
-  console.log("req: " + req.baseUrl + " - " + JSON.stringify(req.query));
+  //console.log("handleExpressRequest:");
+  //console.log("request: " + request.baseUrl + " - " + JSON.stringify(request.query));
 
   // Validate input parameters
   //query string looks like: ?format=[json|csv]&startdate=[whatever]&enddate=[whatever]
-  let format = req.query.format;
+  let format = request.query.format;
   if (!format) {
     format = "json";
   }
   if (!(format === "json" || format === "csv")) {
-    res.send("Bad \"format]\" parameter. Must be \"json\" or \"csv\"");
+    response.send("Bad \"format]\" parameter. Must be \"json\" or \"csv\"");
     return;
   }
 
-  let startDate = req.query.startdate;
+  let startDate = request.query.startdate;
   if (!startDate) {
-    res.send("Missing \"startdate\" parameter.");
+    response.send("Missing \"startdate\" parameter.");
     return;
   }
   //TODO: Parse startDate from string
 
-  let endDate = req.query.enddate;
+  let endDate = request.query.enddate;
   if (!endDate) {
-    res.send("Missing \"enddate\" parameter.");
+    response.send("Missing \"enddate\" parameter.");
     return;
   }
   //TODO: Parse endDate from string
@@ -65,11 +65,11 @@ function handleExpressRequest(req, res) {
     let csv = parser.parse(rptJsonRowsAsJson);
     let fileName = "CommissionReport.csv";
     //TODO: As a user, I'd appreciate a more unique filename with the date range embedded
-    res.setHeader("Content-Disposition", "attachment ; filename=\"" + fileName + "\"");
-    res.contentType("text/csv");
-    res.send(csv);
+    response.setHeader("Content-Disposition", "attachment ; filename=\"" + fileName + "\"");
+    response.contentType("text/csv");
+    response.send(csv);
   } else if (format === "json") {
-    res.json(rptJsonRowsAsJson);
+    response.json(rptJsonRowsAsJson);
   }
 }
 
