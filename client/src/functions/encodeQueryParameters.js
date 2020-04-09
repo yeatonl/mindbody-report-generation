e81f054378f1d92e4e1faee56cea98d6bb5f49cb
Raven <mossygreenrock@gmail.com>
2020-04-09 10:42:16 -0700

    handle empty parameter object

--- client/src/functions/encodeQueryParameters.js
+++ client/src/functions/encodeQueryParameters.js
@@ -1,12 +1,14 @@
 export default function encodeQueryData(url, parameters) {
   let ret = [];
-  for (const [key, value] of Object.entries(parameters)){
-    console.log(parameters);
-    if (value){
-      ret.push(encodeURIComponent(key) + "=" + encodeURIComponent(value));
+  if (parameters){
+    for (const [key, value] of Object.entries(parameters)){
+      console.log(parameters);
+      if (value){
+        ret.push(encodeURIComponent(key) + "=" + encodeURIComponent(value));
+      }
     }
+
+    return url + "?" + ret.join("&");
   }
-
-
-  return url + "?" + ret.join("&");
+  return url;
 }
