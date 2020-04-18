1d87d32c8f2b598c242426529faaa7a73f93151b
Floris Creyf <floris.creyf@gmail.com>
2020-04-18 15:53:55 -0700

    Take time of purchases into consideration
    Instructors now only receive credit for purchases clients make after visiting
    their classes.

--- server/commision.js
+++ server/commision.js
@@ -1,11 +1,14 @@
-const https = require("https");
-const querystring = require("querystring");
-const util = require("util");
-const key = "bfc05de4947f464f85ecd85deff6895e";
+import https from "https";
+import querystring from "querystring";
+import util from "util";
 
-var auth = "5c44ce49f6c8499399f53174536beb2602b74eaf41af4cd29e5df1c3db8db6ba";
+const key = "bfc05de4947f464f85ecd85deff6895e";
+var auth = "bf423f7b7f224ba589d42eb982856640d1e847d7608e4cb5a55f530909fc3c70";
 //var auth = "";
 //printAuthorizationKey("Siteowner", "apitest1234");
+
+//to add client visits: sandbox -> classes -> sign in -> search for client -> add
+//to add sales: sandbox -> retail -> client search -> add item
 
 function getRequest(path, payload, callback) {
   var options = getOptions(path);
@@ -55,6 +58,9 @@ function printAuthorizationKey(username, password) {
 class CommissionReport {
   constructor() {
     this.startDate = "";
+    //a different start date is used to determine all the previous
+    //instructors for a client
+    this.visitStartDate = "2000-01-01T00:00:00";
     this.sales = {};
     this.visits = {};
     this.products = {};
@@ -65,7 +71,7 @@ class CommissionReport {
     this.startDate = date;
   }
 
-  /** Return a promise for the generated report. */
+  //return a promise for the generated report
   generate() {
     return this.requestSales()
       .then(this.requestSaleItems.bind(this))
@@ -73,8 +79,8 @@ class CommissionReport {
       .then(this.merge.bind(this));
   }
 
-  /** Return a promise for the revenue of each instructor.
-  Value format: {staff_name: {service_name/product_name: revenue}} */
+  //return a promise for the revenue of each instructor.
+  //format: {staff_name: {service_name/product_name: revenue}}
   merge() {
     var params = {showHidden: false, depth: null};
     console.log("sales:", util.inspect(this.sales, params));
@@ -84,88 +90,81 @@ class CommissionReport {
 
     return new Promise((resolve, reject) => {
       var staff = {};
-      for (const clientID in this.visits) {
-        this.updateServiceRevenue(staff, clientID);
-        this.updateProductRevenue(staff, clientID);
-      }
+      for (const clientID in this.visits)
+        this.incorporateSales(staff, clientID);
       resolve(staff);
     });
   }
 
-  /** Update the revenue for staff members for a given client. */
-  updateServiceRevenue(staff, clientID) {
-    var visits = this.visits[clientID];
-    for (let i = 0; i < visits.length; i++) {
-      var staffID = visits[i].StaffId;
-      var serviceID = visits[i].ServiceId;
+  //add sales to staff members
+  incorporateSales(staff, clientID) {
+    var sales = this.sales[clientID];
+    for (let i = 0; i < sales.length; i++)
+      this.incorporatePurchases(staff, sales[i], clientID);
+  }
 
-      if (serviceID in this.services == false) {
-        console.warn(`${serviceID} not in services`);
-        continue;
+  //add purchases within a sale to staff members
+  incorporatePurchases(staff, sale, clientID) {
+    var date = Date.parse(sale.SaleDateTime);
+    for (let i = 0; i < sale.PurchasedItems.length; i++) {
+      var purchase = sale.PurchasedItems[i];
+      var staffIDs = this.getPriorInstructors(date, clientID);
+      var item = this.getItem(purchase);
+
+      for (let j = 0; j < staffIDs.length; j++) {
+        var staffID = staffIDs[j];
+        if (staffID in staff == false)
+          staff[staffID] = {};
+
+        if (item.Id in staff[staffID] == false)
+          staff[staffID][item.name] = item.price / staffIDs.length;
+        else
+          staff[staffID][item.name] += item.price / staffIDs.length;
       }
-
-      if (staffID in staff == false)
-        staff[staffID] = {};
-      if (serviceID in staff[staffID] == false)
-        staff[staffID][serviceID] = 0.0;
-
-      var service = this.services[serviceID];
-      if (service.Type == "Unlimited")
-        payment = service.Price / this.getServiceUsage(visits, serviceID);
-      else
-        payment = service.Price / service.Count;
-
-      staff[staffID][serviceID] += payment;
     }
   }
 
-  /** Return the number of times a service was used. */
-  getServiceUsage(visits, serviceID) {
-    var count = 0;
-    for (let i = 0; i < visits.length; i++)
-      if (visits[i].ServiceId == serviceID)
-        count++;
-    return count;
+  //return the name and price of a purchase
+  getItem(purchase) {
+    var item = {
+      name: "",
+      price: 0.0
+    };
+    if (purchase.BarcodeId) {
+      item.name = purchase.BarcodeId;
+      if (purchase.BarcodeId in this.products) {
+        item.name = this.products[purchase.BarcodeId].Name;
+        item.price = this.products[purchase.BarcodeId].Price;
+      } else
+        console.warn(`${purchase.BarcodeId} not in products`);
+    } else {
+      item.name = purchase.Id;
+      if (purchase.Id in this.services) {
+        item.name = this.services[purchase.Id].Name;
+        item.price = this.services[purchase.Id].Price;
+      } else
+        console.warn(`${purchase.Id} not in services`);
+    }
+    return item;
   }
 
-  /** Divide product revenues among staff based on client visits. */
-  updateProductRevenue(staff, clientID) {
+  //return a list of staff IDs that the client visited before a date
+  getPriorInstructors(saleDate, clientID) {
     var staffIDs = [];
     var visits = this.visits[clientID];
     for (let i = 0; i < visits.length; i++) {
       var staffID = visits[i].StaffId;
-      staffIDs.push(staffID);
-      if (staffID in staff == false)
-        staff[staffID] = {};
+      var classDate = Date.parse(visits[i].StartDateTime);
+      if (saleDate > classDate)
+        staffIDs.push(staffID);
     }
-
-    var sales = this.sales[clientID];
-    for (let i = 0; i < sales.length; i++) {
-      var purchases = sales[i].PurchasedItems;
-      for (let j = 0; j < purchases.length; j++)
-        if (purchases[j].BarcodeId in this.products)
-          this.distributeProductRevenue(
-            staff, purchases[j].BarcodeId, staffIDs);
-    }
+    //still mention purchases even if there was no previous instructor
+    if (staffIDs.length == 0)
+      staffIDs.push(0);
+    return staffIDs;
   }
 
-  /** Distribute revenue for a product among instructors. */
-  distributeProductRevenue(staff, productID, staffIDs) {
-    if (productID in this.products == false) {
-      console.warn(`${productID} not in products`);
-      return;
-    }
-
-    var price = this.products[productID].Price;
-    for (let i = 0; i < staffIDs.length; i++) {
-      var staffID = staffIDs[i];
-      if (productID in staff[staffID] == false)
-        staff[staffID][productID] = 0.0;
-      staff[staffID][productID] += price / staffIDs.length;
-    }
-  }
-
-  /** Return a promise for all sale items. */
+  //return a promise for all sale items
   requestSaleItems() {
     var serviceIDs = new Set();
     var productIDs = new Set();
@@ -187,7 +186,8 @@ class CommissionReport {
     ]);
   }
 
-  /** Return sales for each client from a list of all sales. */
+  //return sales for each client from a list of all sales.
+  //format: {client: sales}
   parseSales(sales) {
     var clients = {};
     for (let i = 0; i < sales.length; i++) {
@@ -199,13 +199,13 @@ class CommissionReport {
     return clients;
   }
 
-  /** Return a promise for all product and service sales. */
+  //return a promise for all product and service sales
   requestSales() {
     var self = this;
     return new Promise((resolve, reject) => {
       var payload = {
         StartDateTime: this.startDate,
-        PaymentMethodId: 1 // REMOVE
+        PaymentMethodId: 1 //REMOVE
       };
       getRequest("sale/sales", payload, resp => {
         var data = "";
@@ -219,7 +219,7 @@ class CommissionReport {
     });
   }
 
-  /** Return a promise for all visit information using client sales. */
+  //return a promise for all visit information using client sales
   requestVisits() {
     var clientVisits = [];
     for (const clientID in this.sales)
@@ -227,13 +227,13 @@ class CommissionReport {
     return Promise.all(clientVisits);
   }
 
-  /** Return a promise for visit information of a single client. */
+  //return a promise for visit information of a single client
   requestClientVisits(clientID) {
     var self = this;
     return new Promise((resolve, reject) => {
       var payload = {
         ClientId: clientID,
-        StartDateTime: this.startDate
+        StartDateTime: this.visitStartDate
       };
       getRequest("client/clientvisits", payload, resp => {
         var data = "";
@@ -247,7 +247,7 @@ class CommissionReport {
     });
   }
 
-  /** Return a promise for service pricing info. */
+  //return a promise for service pricing info
   requestServices(services) {
     var self = this;
     return new Promise((resolve, reject) => {
@@ -263,7 +263,7 @@ class CommissionReport {
     });
   }
 
-  /** Return a promise for product pricing info. */
+  //return a promise for product pricing info
   requestProducts(products) {
     var self = this;
     return new Promise((resolve, reject) => {
@@ -279,7 +279,7 @@ class CommissionReport {
     });
   }
 
-  /** Return a dictionary for easier product lookups. */
+  //return a dictionary for easier product lookups
   productListToDict(products) {
     var result = {};
     for (let i = 0; i < products.length; i++)
@@ -291,67 +291,3 @@ class CommissionReport {
 var report = new CommissionReport();
 report.setStartDate("2020-04-09T00:00:00");
 report.generate().then(staff => console.log(staff));
-
-//getRequest("sale/sales", {
-//    Limit:200,
-//    PaymentMethodId:1
-//    StartSaleDateTime:"2020-04-09T12:00:00Z",
-//    EndSaleDateTime:"2020-04-09T23:59:00Z"
-//}, resp => {
-//  var data = "";
-//  resp.on("data", chunk => data += chunk);
-//  resp.on("end", () => {
-//    console.log(JSON.parse(data));
-//    var sales = JSON.parse(data).Sales;
-//    for (let i = 0; i < sales.length; i++)
-//      console.log(sales[i]);
-//  });
-//});
-//
-//getRequest("sale/products", {}, resp => {
-//  var data = "";
-//  resp.on("data", chunk => data += chunk);
-//  resp.on("end", () => {
-//    var x = JSON.parse(data).Products;
-//    for (let i = 0; i < x.length; i++)
-//      console.log(x[i]);
-//  });
-//});
-//
-//getRequest("sale/services", {}, resp => {
-//  var data = "";
-//  resp.on("data", chunk => data += chunk);
-//  resp.on("end", () => {
-//    var x = JSON.parse(data).Services;
-//    for (let i = 0; i < x.length; i++)
-//      console.log(x[i]);
-//  });
-//});
-//
-//var cart = {
-//  Test: false,
-//  ClientId: 100014871,
-//  Items: [
-//    {Item: {Type: "Product", Metadata: {Id: "0001"}}, Quantity: 22},
-//    {Item: {Type: "Product", Metadata: {Id: "0002"}}, Quantity: 6}
-//  ],
-//  InStore: true,
-//  Payments: [
-//    {
-//      Type: "Cash",
-//      Metadata: {
-//        Amount: 53.52,
-//        Notes: "Tip"
-//      }
-//    }
-//  ],
-//  LocationId: 1
-//};
-//
-//postRequest("sale/checkoutshoppingcart", cart, resp => {
-//  var data = "";
-//  resp.on("data", chunk => data += chunk);
-//  resp.on("end", () => {
-//    console.log(JSON.parse(data));
-//  });
-//});
