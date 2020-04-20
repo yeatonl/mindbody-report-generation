681d970d1924aac3fcabce6acafa6f5181307f38
Daniel Brewer <44036901+DanielBrewerPsu@users.noreply.github.com>
2020-04-19 18:44:05 -0700

    Connected the commission report endpoint to the reporting logic
     - Restored Floris' code for adding sales data. I'm still using it
    for testing.
     - Added some checks to handle when Mindbody returns nulls for empty lists.
     - Modified commission-report code to display actual results instead of
    dummy data. At this point, the StaffNames are opaque ID numbers and not
    readable names.

--- server/reports/commission/commission-report.js
+++ server/reports/commission/commission-report.js
@@ -1,6 +1,7 @@
 //module "commission-report"
 import json2csvExports from "json2csv";
 import {printAuthorizationKey, CommissionReport} from "../../commision.js";
+import MindbodyAccess from "../../api-manager.js";
 
 /*a handy request URLS for testing:
 http://localhost:8080/reports/commission?format=csv&startdate=2020-03-01T00:00:00&enddate=2020-03-031T00:00:00
@@ -9,7 +10,6 @@ export function handleCommissionRptRequest(request, response) {
 
   //console.log("handleCommissionRptRequest:");
   //console.log("request: " + request.baseUrl + " - " + JSON.stringify(request.query));
-  //printAuthorizationKey("Siteowner", "apitest1234");
 
   //validate input parameters
   //query string looks like: ?format=[json|csv]&startdate=[whatever]&enddate=[whatever]
@@ -28,64 +28,72 @@ export function handleCommissionRptRequest(request, response) {
     response.send("Missing \"startdate\" parameter.");
     return;
   }
-  //todo: Parse startDate from string
-
+  
   let endDate = request.query.enddate;
   if (!endDate) {
     response.send("Missing \"enddate\" parameter.");
     return;
   }
-  //todo: Parse endDate from string
-
+  
 
   //pass validated input params to report generator
   let rptGenerator = new CommissionReport();
   rptGenerator.setStartDate(startDate);
   //rptGenerator.setEndDate(endDate);
+  //rptGenerator.addFakeSalesDataForTesting();
 
   rptGenerator.generate().then((results) => {
-    console.log("CommissionReport.generate() returned this data: ");
-    console.log(JSON.stringify(results));
+    //console.log("CommissionReport.generate() returned this data: ");
+    //console.log(JSON.stringify(results));
     // Results look like this: {"100000283":{"123456789":1.25}}
-    // which I think is: {staffID: {productSold: commissionAmount}}
-    //todo: lookup staffName from ID.
-    //aggregate sale amounts for each staff entry
-    //aggregate other commission amount for each staff entry
+    // which I think is: {staffID: {productSold: commissionAmount, secondProduct: secondAmount}}
 
-    let rptJsonRowsAsJson = [
-      {
-        "paramName": "Key:",
-        "value": "Value:",
-      },
-      {
-        "paramName": "format",
-        "value": format,
-      },
-      {
-        "paramName": "startDate",
-        "value": startDate,
-      },
-      {
-        "paramName": "endDate",
-        "value": endDate,
-      },
-    ];
-    //^^^ todo: Replace this dummy data.
+    //lookup staff names from the IDs returned by CommissionReport
+    MindbodyAccess.getAuth()
+    .then(MindbodyAccess.getStaff({StaffIds: Object.keys(results)}))
+    .then(staffResponse => {
+      /* The response from MindbodyAccess is currently: {"size":0,"timeout":0},
+      so we have no valid StaffMembers to interact with here.
+      let staffIdToNameDict = {};
+      for (let staffInfo in staffResponse.StaffMembers) {
+        staffIdToNameDict[staffInfo.Id] = staffInfo.FirstName + " " + staffInfo.LastName;
+      }
+      */
 
-    //generate output
-    if (format === "csv") {
-      let fields = ["paramName", "value"];
-      let parser = new json2csvExports.Parser({fields});
-      let csv = parser.parse(rptJsonRowsAsJson);
-      let fileName = "CommissionReport.csv";
-      //todo: As a user, I'd appreciate a more unique filename with the date range embedded
-      response.setHeader("Content-Disposition", "attachment ; filename=\"" + fileName + "\"");
-      response.contentType("text/csv");
-      response.send(csv);
-    } else if (format === "json") {
-      response.json(rptJsonRowsAsJson);
-    }
+      //generate output
+      if (format === "csv") {
+        let csvRows = [];
+        let totalCommission = 0.0;
+        for (let staffId in results) {
+          //the value for results[staffId] is a dict of commission amounts that we must now total
+          let commission = 0.0;
+          for (let item in results[staffId]) {
+            commission += results[staffId][item];
+          }
+          csvRows.push({
+            "Instructor": staffId,  // staffIdToNameDict[staffId]
+            "Commission": commission,
+          });
+          totalCommission += commission
+        }
+        // Additionally, add an extra row for totals
+        csvRows.push({
+            "Instructor": "Total",
+            "Commission": totalCommission,
+        });
 
+        let fields = ["Instructor", "Commission"];
+        let parser = new json2csvExports.Parser({fields});
+        let csv = parser.parse(csvRows);
+        let fileName = "CommissionReport.csv";
+        //todo: As a user, I'd appreciate a more unique filename with the date range embedded
+        response.setHeader("Content-Disposition", "attachment ; filename=\"" + fileName + "\"");
+        response.contentType("text/csv");
+        response.send(csv);
+      } else if (format === "json") {
+        response.json(results);
+      }
+    })
   });
 }
 
