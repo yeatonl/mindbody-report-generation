0bda581d6586e9136c9dfb2f32c9f57dd8f4dfd6
Asher Roper <roper@pdx.edu>
2020-04-27 12:28:42 -0700

    commented code. Should now be understandable

--- server/reports/attendance/attendance-endpoint.js
+++ server/reports/attendance/attendance-endpoint.js
@@ -1,28 +1,35 @@
 import MindbodyAccess from "../../api-manager";
 import J2C from "json2csv";
 
-//returns the current date without slashes
+//returns the current date 
 function getDate() {
   let today = new Date();
   let dd = String(today.getDate()).padStart(2, '0');
   let mm = String(today.getMonth() + 1).padStart(2, '0');
   let yyyy = today.getFullYear();
-  today = mm + "-" + dd + "-" + yyyy; //current date without slashes 
+  today = mm + "/" + dd + "/" + yyyy; 
   return today;
 }
 
-//returns true if "toCheck" date is between (or equivalent to) start and end dates.
-//all parameters are strings in the form of mm/dd/yyyy
-function isValidDate(start, end, toCheck) {
-  //TODO: use the next two lines of code if I decide to change check date format within the function
-  let toCheckSplit = toCheck.split(/[-\/TU]/);
-  let fixedCheck = toCheckSplit[1] + "/" + toCheckSplit[2] + "/" + toCheckSplit[0]; //note variable name isn't toCheck.
-  let startDate = Date.parse(start); //I guess Date.parse handles various common date delimeters including - and / and empty space
+// takes a date as a string in the form yyyy/mm/dd (Mindbodys default date format) and
+// returns a date as a string in the form mm/dd/yyyy
+function fixDateFormat(date) {
+  let dateSplit = date.split(/[-.\/TU]/);
+  let fixedDate = dateSplit[1] + "/" + dateSplit[2] + "/" + dateSplit[0]; 
+  return fixedDate;
+}
+
+// returns true if start <= toCheck <= end
+// all parameters are dates as strings in the form mm/dd/yyyy 
+function isValidDate(start, toCheck, end) {
+  let startDate = Date.parse(start); 
   let endDate = Date.parse(end);
-  let checkDate = Date.parse(fixedCheck);
+  let checkDate = Date.parse(toCheck);
   return (checkDate >= startDate && checkDate <= endDate) ? true : false;
 }
 
+// takes a classID as a parameter and 
+// returns the number of students that attended that class
 function getNumberAttended(classID) {
   return new Promise ((resolve, reject) => {
     MindbodyAccess.getClassVisits({ClassID: classID})
@@ -33,72 +40,80 @@ function getNumberAttended(classID) {
     })
 }
 
+// gets attendance data from mindbody endpoints and outputs it as .csv or JSON to our endpoint
+// endpoint URL example
+// http://localhost:8080/reports/attendance?format=csv&startdate=01/01/2020&enddate=12/31/2020
 export function attendanceRequestHandler(request, response) {
-  //endpoint URL example
-  //http://localhost:8080/reports/attendance?format=csv&startdate=01/01/2020&enddate=12/31/2020
   let format = request.query.format; //gets format value in URL query
   let startdate = request.query.startdate; //gets startdate value in URL query
   let enddate = request.query.enddate; //gets enddate value in URL query
 
-  //"Throws error" if format isn't JSON or CSV
+  // error handling
+  // "throws error" if format isn't JSON or CSV
   if (!(format === "json" || format === "csv")) {
     response.send('Bad format parameter. Must be "json" or "csv"');
     return;
   }
-  //Sets startdate to current day if field is NULL
+  // sets startdate to current day if field is NULL
   if (!startdate) {
     startdate = getDate();
   }
-  //Sets enddate to current day if field is NULL
+  // sets enddate to current day if field is NULL
   if (!enddate) {
     enddate = getDate();
   }
 
+  // access Mindbody Endpoints
   MindbodyAccess.getAuth()
     .then((value) => {
       MindbodyAccess.authToken = value.AccessToken;
       return MindbodyAccess.getClasses();
     })
+    // gets all classes from "Classes" endpoint
     .then((classes) => {
       let attendanceReport = [];
+      let allNumberAttendedPromises = [];
       let numberOfClasses = Object.keys(classes.Classes).length;
-      // fills classData object with necessary information then pushes it to attendanceReport list
-      let allPromises = [];
+
+      // iterates through every class in classes
       for (let i = 0; i < numberOfClasses; ++i) {
-        let classID = classes.Classes[i].Id; // unique class ID
-        let maxCapacity = classes.Classes[i].MaxCapacity; // maximum number of students that can enroll for that class
-        let numberRegistered = classes.Classes[i].TotalBooked; // number of students who registered for that class
-        let classDate = classes.Classes[i].StartDateTime; // startDateTime format is yyyy/mm/dd by default. Altered in isValidDate
-        if (isValidDate(startdate, enddate, classDate)) {
+        let classID = classes.Classes[i].Id;
+        let maxCapacity = classes.Classes[i].MaxCapacity;
+        let numberRegistered = classes.Classes[i].TotalBooked;
+        let classDate = fixDateFormat(classes.Classes[i].StartDateTime); // Mindbody StartDateTime format is yyyy/mm/dd by default
+
+        // adds class data to attendance report if startdate <= classdate <= enddate
+        if (isValidDate(startdate, classDate, enddate)) {
           let classData = {
             class: classID,
             capacity: maxCapacity,
             registered: numberRegistered,
           };
-          let myPromise = getNumberAttended(classID)
+          // adds attendance parameter to classData
+          let numberAttendedPromise = getNumberAttended(classID)
             .then((numberAttended) => {
-              //console.log(numberAttended);
               classData["attended"] = numberAttended;
-              attendanceReport.push(classData);
             })
             .catch((err) => {
               console.log("ERROR! ", err);
-            })
-          allPromises.push(myPromise);
+            });
+          allNumberAttendedPromises.push(numberAttendedPromise);
+          attendanceReport.push(classData); // pushes current class's data to attendanceReport
         }
       }
-      Promise.all(allPromises).then(() => {
-        // outputs data as .csv
-        if (format === "csv") {
-          let fields = ["class", "capacity", "registered", "attended"];
-          let parser = new J2C.Parser({ fields });
-          let csv = parser.parse(attendanceReport);
-          response.send(csv);
-          //outputs data as json
-        } else {
-          response.json(attendanceReport);
-        }
-      });
+
+      // resolves all, allNumberAttendedPromises then outputs attendance report to endpoint as CSV or JSON
+      Promise.all(allNumberAttendedPromises)
+        .then(() => {
+          if (format === "csv") {
+            let fields = ["class", "capacity", "registered", "attended"];
+            let parser = new J2C.Parser({ fields });
+            let csv = parser.parse(attendanceReport);
+            response.send(csv);
+          } else {
+            response.json(attendanceReport);
+          }
+        });
     })
     .catch((m) => {
       //TODO: make an error message consistent with commission report
