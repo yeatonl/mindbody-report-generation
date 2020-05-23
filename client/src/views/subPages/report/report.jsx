706f30b2a237dd110b3d8c2ac36f17f3e062fdc7
Raven <mossygreenrock@gmail.com>
2020-05-23 10:59:27 -0700

    Enable download button

--- client/src/views/subPages/report/report.jsx
+++ client/src/views/subPages/report/report.jsx
@@ -53,11 +53,11 @@ export default connect((state) => {
   }
 
   downloadData = () => {
-    //tbd
+    let url = Urls.API_BASE + encodeQueryParameters(this.props.report.csvEndpoint, this.state.parametersData);
+    window.open(url, "_blank");
   }
 
   loadDataFromEndpoint = () => {
-    console.log(this.props.report);
     let url = encodeQueryParameters(this.props.report.jsonEndpoint, this.state.parametersData);
     Actions.fetchReportData(url, this.props.report.key);
   }
@@ -85,19 +85,18 @@ export default connect((state) => {
             />
             <Button
               ghost
+              label="Download"
+              tempLabel="Downloaded"
+              onClick={this.downloadData}
+              title="Download as a CSV file"
+            />
+            <Button
+              ghost
               label="Copy"
               tempLabel="Copied"
               disabled={!(data && data.length > 0)}
               onClick={this.copyDataToClipboard}
               title="Copy the CSV data to clipboard"
-            />
-            <Button
-              ghost
-              label="Download"
-              tempLabel="Downloaded"
-              disabled={!(data && length > 0)}
-              onClick={this.downloadData}
-              title="Download as a CSV file"
             />
             <IconButton
               className={this.state.showParameterDocumentation ? "active" : ""}
