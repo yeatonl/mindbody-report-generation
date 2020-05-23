24be07fff853975d652b0a30e8496ed3de139eee
Raven <mossygreenrock@gmail.com>
2020-05-23 11:09:25 -0700

    Fix unfocus

--- client/src/functions/unfocus.js
+++ client/src/functions/unfocus.js
@@ -1,3 +1,7 @@
-export default function unfocus(){
-  document.activeElement.blur();
+export default function unfocus(event){
+  console.log("A");
+  if (event.target === document.activeElement){
+    console.log("B");
+    document.activeElement.blur();
+  }
 }
